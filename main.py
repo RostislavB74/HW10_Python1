d4ee327a68d3366ceea07300f607d769ceb8f73d
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-07-09 11:48:14 +0300

    edit

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
+from collections import UserDict
+
+
+class AddressBook(UserDict):
+    pass
+
+
+class Record():
+   def __repr__(self):
+       return str(.....)
+
+
+class Field():
+    pass
+
+
+class Name():
+    pass
+
+
+class Phone():
+    pass
+
+
 if __name__ == "__main__":
-    
+
     ab = AddressBook()
     name = Name("Bill")
     phone1 = Phone("12345")
     rec = Record(name, phone1)
     print(rec.name)
-    
+
     ab.add_record(rec)
-    
+
     ab.add_record(Record(Name("Jill")))
-    
+
     for rec in ab.values():
         assert isinstance(rec, Record)
-    
-    phone2 = Phone("56784")
-    print(ab)
+
+        phone2 = Phone("56784")
+        print(ab)
+
+
+# reсords = [Record(...), Record(...) ]
+# return = "\n".join(reсords)
+# print (return)
+# result = map(str, reсords)
+# return = "\n".join(result)
