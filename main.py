1db26908da596147dfca76ef3a11109af2a4d992
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-07-10 22:00:45 +0300

    edit

--- main.py
+++ main.py
@@ -2,11 +2,28 @@ from collections import UserDict
 
 
 class AddressBook(UserDict):
-    def add_record(rec):
-        pass
+    contacts = []
 
 
 class Record():
+
+    def add_record(self, *args):
+        name = Name(args[0])
+        phone = Phone(args[1])
+        rec = Record(name, phone)
+        print(rec)
+        ab.append(rec)
+        return ab.add_rec(rec)
+
+    def remove_record(self):
+        pass
+
+    def edit_field(self):
+        pass
+
+    def save_name(self):
+        pass
+
     def __init__(self, name, num_phone):
         self.name = name
         self.phone = num_phone
@@ -38,16 +55,14 @@ if __name__ == "__main__":
     phone1 = Phone("12345")
     rec = Record(name, phone1)
     print(rec.name)
+    res = ab.contacts.append(Record.add_record(rec))
+    print(res)
 
-    ab.add_record(rec)
+    # for rec in ab.values():
+    #     assert isinstance(rec, Record)
 
-    ab.add_record(Record(Name("Jill")))
-
-    for rec in ab.values():
-        assert isinstance(rec, Record)
-
-        phone2 = Phone("56784")
-        print(ab)
+    #     phone2 = Phone("56784")
+    #     print(ab)
 
 
 # reсords = [Record(...), Record(...) ]
@@ -55,3 +70,29 @@ if __name__ == "__main__":
 # print (return)
 # result = map(str, reсords)
 # return = "\n".join(result)
+# def __init__(self):
+#         self.contacts = []
+
+
+#     def list_contacts(self, contacts):
+
+#         return self.contacts
+
+
+#     def add_contacts(self, name, phone, email, favorite=False):
+#         contacts = []
+#         ab={}
+#         ab['id']= Contacts.current_id
+#         ab['name'] = name
+#         ab['phone'] = phone
+#         ab['email'] = email
+#         ab['favorite'] = favorite
+#         contacts.append(ab)
+
+#         print (contacts)
+
+#         Contacts.current_id+=1
+#         print(Contacts.current_id)
+#         return contacts
+
+# print (Contacts.list_contacts(Contacts.add_contacts(1,"Wylie Pope", "(692) 802-2949", "est@utquamvel.net", True)))
