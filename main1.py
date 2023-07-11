7aabd843fe89cc1429bf10f9c57ef4e8d7109520
Rostyslav Bysko <brn.h74@gmail.com>
2023-07-11 09:30:59 +0300

    edit

--- main1.py
+++ main1.py
@@ -1,7 +1,6 @@
 address_book = {}
 
 
-<<<<<<< HEAD
 def input_error(func):
     def wrapper(*args, **kwargs):
         try:
@@ -21,39 +20,6 @@ def add(*args):
     phone = args[1]
     address_book[str.capitalize(name)] = phone
     return f"Add success {name} {phone}"
-=======
-class AddressBook(UserDict):
-    contacts = []
-
-
-class Record():
-
-    def add_record(self, *args):
-        name = Name(args[0])
-        phone = Phone(args[1])
-        rec = Record(name, phone)
-        print(rec)
-        ab.append(rec)
-        return ab.add_rec(rec)
-
-    def remove_record(self):
-        pass
-
-    def edit_field(self):
-        pass
-
-    def save_name(self):
-        pass
-
-    def __init__(self, name, num_phone):
-        self.name = name
-        self.phone = num_phone
-
-    def __repr__(self, records):
-        # result = map(str, reсords)
-        return "\n".join(str(rec) for rec in records)
-        #   return str(.....)
->>>>>>> 1db26908da596147dfca76ef3a11109af2a4d992
 
 
 @input_error
@@ -99,7 +65,7 @@ def no_command(*args, **kwargs):
     return "Unknown command"
 
 
-def parser(text: str): #
+def parser(text: str): #-> tuple[callable, tuple[str] | None]:
 
     if text:
         text1 = text.lower()
@@ -129,54 +95,4 @@ def show_all(*args):
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     main()
-=======
-
-    ab = AddressBook()
-    name = Name("Bill")
-    phone1 = Phone("12345")
-    rec = Record(name, phone1)
-    print(rec.name)
-    res = ab.contacts.append(Record.add_record(rec))
-    print(res)
-
-    # for rec in ab.values():
-    #     assert isinstance(rec, Record)
-
-    #     phone2 = Phone("56784")
-    #     print(ab)
-
-
-# reсords = [Record(...), Record(...) ]
-# return = "\n".join(reсords)
-# print (return)
-# result = map(str, reсords)
-# return = "\n".join(result)
-# def __init__(self):
-#         self.contacts = []
-
-
-#     def list_contacts(self, contacts):
-
-#         return self.contacts
-
-
-#     def add_contacts(self, name, phone, email, favorite=False):
-#         contacts = []
-#         ab={}
-#         ab['id']= Contacts.current_id
-#         ab['name'] = name
-#         ab['phone'] = phone
-#         ab['email'] = email
-#         ab['favorite'] = favorite
-#         contacts.append(ab)
-
-#         print (contacts)
-
-#         Contacts.current_id+=1
-#         print(Contacts.current_id)
-#         return contacts
-
-# print (Contacts.list_contacts(Contacts.add_contacts(1,"Wylie Pope", "(692) 802-2949", "est@utquamvel.net", True)))
->>>>>>> 1db26908da596147dfca76ef3a11109af2a4d992
--- main_new.py
+++ main_new.py
@@ -7,9 +7,35 @@ class AddressBook(UserDict):
 
 
 class Record():
+    
     def __init__(self, name, num_phone):
         self.name = name
         self.phone = num_phone
+
+
+class Parser():
+
+    def parser(text: str): #-> tuple[callable, tuple[str] | None]:
+
+        if text:
+            text1 = text.lower()
+            if text1.startswith("add"):
+                return add, text1.replace("add", "").strip().split()
+            if text1.startswith("hello"):
+                return hello, "How can I help you?"
+            if text1.startswith("close") or text1.startswith("exit") or text1.startswith("good bye"):
+                return exit, "Good bye!"
+            if text1.startswith("show all"):
+                return show_all, 'show all'
+            if text1.startswith("phone"):
+                return get_phone, text.replace("phone", "").strip().split()
+            if text1.startswith("change"):
+                return change, text.replace("change", "").strip().split()
+            else:
+                return no_command, ''
+        else:
+            return no_command, ''
+
 
     def __repr__(self, records):
         # result = map(str, reсords)
@@ -26,28 +52,55 @@ class Name(Field):
         self.name = data
 
 
+class Main():
+    def main():
+        while True:
+            user_input = input(">>>")
+            if user_input:
+                command, data = Parser.parser(user_input)
+                result = command(*data)
+                if result:
+                    if result == "Good bye!":
+                        print(result)
+                        break
+                    print(result)
+                continue
+            else:
+                print(no_command())
+
+
+def no_command(*args, **kwargs):
+    return "Unknown command"
+
+
 class Phone(Field):
     def __init__(self, num_phone):
         self.phone = num_phone
 
 
+
 if __name__ == "__main__":
+    Main.main()
 
-    ab = AddressBook()
-    name = Name("Bill")
-    phone1 = Phone("12345")
-    rec = Record(name, phone1)
-    print(rec.name)
 
-    ab.add_record(rec)
 
-    ab.add_record(Record(Name("Jill")))
 
-    for rec in ab.values():
-        assert isinstance(rec, Record)
 
-        phone2 = Phone("56784")
-        print(ab)
+# ab = AddressBook()
+# name = Name("Bill")
+# phone1 = Phone("12345")
+# rec = Record(name, phone1)
+# print(rec.name)
+
+# ab.add_record(rec)
+
+# ab.add_record(Record(Name("Jill")))
+
+# for rec in ab.values():
+#     assert isinstance(rec, Record)
+
+#     phone2 = Phone("56784")
+#     print(ab)
 
 
 # reсords = [Record(...), Record(...) ]
