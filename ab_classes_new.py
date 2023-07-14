e26e2f93e9c44061f9de4812b0fd8616b8444083
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-07-14 20:49:50 +0300

    ok

--- ab_classes_new.py
+++ ab_classes_new.py
@@ -1,19 +1,20 @@
 from collections import UserDict
 
+
 class Field:
     def __init__(self, value) -> None:
         self.value = value
-    
+
     def __str__(self) -> str:
         return self.value
-    
+
     def __repr__(self) -> str:
         return str(self)
-        
+
 
 class Name(Field):
     ...
-    
+
 
 class Phone(Field):
     ...
@@ -24,21 +25,24 @@ class Record:
         self.name = name
         self.phones = []
         if phone:
-            self.phones.append(phone)
-    
+            if isinstance(phone, str):
+                self.phones.append(phone)
+            if isinstance(phone, list):
+                self.phones = phone
+
     def add_phone(self, phone: Phone):
         if phone.value not in [p.value for p in self.phones]:
             self.phones.append(phone)
             return f"phone {phone} add to contact {self.name}"
         return f"{phone} present in phones of contact {self.name}"
-    
+
     def change_phone(self, old_phone, new_phone):
         for idx, p in enumerate(self.phones):
             if old_phone.value == p.value:
                 self.phones[idx] = new_phone
                 return f"old phone {old_phone} change to {new_phone}"
         return f"{old_phone} not present in phones of contact {self.name}"
-    
+
     def __str__(self) -> str:
         return f"{self.name}: {', '.join(str(p) for p in self.phones)}"
 
